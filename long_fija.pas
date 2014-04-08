335adf7d9586da4b805db47fce588a64940a8400
Ivan Karl <ivan6258@gmail.com>
2014-04-08 20:41:31 -0300

    unit compila
    el unit compila
    agrega archivos innecesarios al gitignore

--- long_fija.pas
+++ long_fija.pas
@@ -1,50 +1,78 @@
-UNIT vieja;
+UNIT long_fija;
 
 Interface
   const
-    corte : -1;
-
-  type export = vieja;
-
-  Procedure Cargar(var a:vieja; nombre:String; dni:Integer; f_nac:Longword);
-  Procedure CargarDesdeRaux(var a:vieja);
-  Procedure Primero(var a:vieja;);
-  Procedure Siguiente(var a:vieja);
-  Procedure Recuperar(var a:vieja; dni:Integer;);
-  Procedure Exportar(var a:vieja; nom_arch_txt:String;);
-  Procedure Insertar(var a:vieja; nombre:String; dni:Integer; fecha:Longword);
-  Procedure Eliminar(var a:vieja; dni:Integer);
-  (* Procedure Modificar(); *)
-  (* Procedure Respaldar(); *)
-  Function fallo_ultima_operacion(var a:vieja): Boolean;
-
-Implementation (* parte privada *)
+    corte = -1;
   type
-    r_persona = Record
-      valido : Boolean of
-        true:(
-          nombre : String;
-          dni    : Integer;
-          f_nac  : Longword); (* AAAAMMDD *)
-        false:(
-          prox  : Longword );
+    r_persona = Packed Record
+      Case valido : Boolean of
+        True: ( nombre : String;
+                dni    : Integer;
+                f_nac  : Longword); (* AAAAMMDD *)
+        False:(  prox  : Longword );
     end;
 
     f_persona = File of r_persona;
 
-
     r_control = Record
       a_persona : f_persona;
       r_aux     : r_persona;
-      libre     : Longword;
       fallo     : Boolean;
+      libre     : Longword;
       (* TODO: preguntar si es lo que esperan devolver un nro de error *)
     end;
-    vieja = r_control; {cuerpo del tipo exportado por el TAD}
+    (* long_fija = r_control; {cuerpo del tipo exportado por el TAD} *)
 
+  Procedure Cargar(var a:r_control; nombre:String; dni:Integer; f_nac:Longword);
+  Procedure CargarDesdeRaux(var a:r_control);
+  Procedure Primero(var a:r_control);
+  Procedure Siguiente(var a:r_control);
+  Procedure Recuperar(var a:r_control; dni:Integer);
+  Procedure Exportar(var a:r_control; nom_arch_txt:String);
+  Procedure Insertar(var a:r_control; nombre:String; dni:Integer; fecha:Longword);
+  Procedure Eliminar(var a:r_control; dni:Integer);
+  Procedure Modificar(var a:r_control; n_nombre:String; dni:Integer; n_f_nac:Longword);
+  Procedure Respaldar(var a:r_control; n_archivo:String);
+  Function fallo_ultima_operacion(var a:r_control): Boolean;
 
-  Procedure Cargar(var a:vieja; nombre:String; dni:Integer; f_nac:Longword);
-  var
+Implementation
+  (* ========================================================= *)
+  (* =====================Privados============================ *)
+  (* ========================================================= *)
+
+  Function fallo_ultima_operacion(var a:r_control): Boolean;
+  begin
+    fallo_ultima_operacion := a.fallo;
+  end;
+
+  procedure leer_registro(var a:f_persona; var r:r_persona);
+  begin
+    if (not eof(a)) then begin
+      read(a, r);
+    end else begin
+      r.dni := corte;
+    end;
+  end;
+  procedure escupir_a_txt(var txt:Text; reg:r_persona);
+  begin
+    with (reg) do begin
+      write(txt, dni);
+      write(txt, f_nac);
+      writeln(txt, nombre);
+      (* TODO: ver si no se puede hacer todo en un solo writeln *)
+    end;
+  end;
+  Procedure negar_registro(var r:r_persona; prox:Longword);
+  begin
+    r.valido := false;
+    r.prox   := prox;
+  end;
+
+  (* ========================================================= *)
+  (* =====================Publicos============================ *)
+  (* ========================================================= *)
+
+  Procedure Cargar(var a:r_control; nombre:String; dni:Integer; f_nac:Longword);
   begin
     (* TODO: chequear q vaya al final *)
     with a do begin
@@ -59,8 +87,7 @@ Implementation (* parte privada *)
     end;
   end;
 
-  Procedure CargarDesdeRaux(var a:vieja;);
-  var
+  Procedure CargarDesdeRaux(var a:r_control);
   begin
     with a do begin
       seek(a_persona, filesize(a_persona)-1);
@@ -69,7 +96,7 @@ Implementation (* parte privada *)
     end;
   end;
 
-  Procedure Primero(var a:vieja);
+  Procedure Primero(var a:r_control);
   begin
     with a do begin
       reset(a_persona);
@@ -78,15 +105,18 @@ Implementation (* parte privada *)
     end;
   end;
 
-  procedure Siguiente(var a:vieja);
+  procedure Siguiente(var a:r_control);
   begin
     with a do begin
       leer_registro(a_persona, r_aux);
+      while (not r_aux.valido) do begin
+        leer_registro(a_persona, r_aux);
+      end;
       fallo := (r_aux.dni = corte);
     end;
   end;
 
-  procedure Recuperar(var a:vieja; dni:Integer;);
+  procedure Recuperar(var a:r_control; dni:Integer);
   var
     encontre : Boolean;
   begin
@@ -105,7 +135,7 @@ Implementation (* parte privada *)
     end;
   end;
 
-  Procedure Exportar(var a:vieja; nom_arch_txt:String;);
+  Procedure Exportar(var a:r_control; nom_arch_txt:String);
   var
     txt : Text;
   begin
@@ -116,13 +146,16 @@ Implementation (* parte privada *)
       while not eof(a_persona) do begin
         (* TODO: preguntar si estaria mejor usar otro reg para no pisar r_aux *)
         read(a_persona, r_aux);
-        escupir_a_txt(txt, r_aux);
+        if (r_aux.valido) then begin
+          escupir_a_txt(txt, r_aux);
+        (* TODO: preguntar como validar si registro de archivo valido|falso *)
+        end;
       end;
       Close(txt);
     end;
   end;
 
-  Procedure Insertar(var a:vieja; nombre:String; dni:Integer; fecha:Longword);
+  Procedure Insertar(var a:r_control; nombre:String; dni:Integer; fecha:Longword);
   begin
     Recuperar(a, dni);
     if (fallo_ultima_operacion(a)) then begin
@@ -132,62 +165,51 @@ Implementation (* parte privada *)
     end;
   end;
 
-  Procedure Eliminar(var a:vieja; dni:Integer);
+  Procedure Eliminar(var a:r_control; dni:Integer);
+  var
+    pos_eliminar : Longword;
   begin
     Recuperar(a, dni);
     (* TODO: rever tuti con cito *)
     if (not fallo_ultima_operacion(a)) then begin
-      seek(a.a_persona, filePos(a.a_persona)-1);
-      write(a.a_persona, a.libre);
-      a.libre := filePos(a.a_persona)-1;
-      reset(a_persona);
-      write(a_persona, a.libre);
-      close(a_persona);
-      a.fallo := false;
+      with (a) do begin
+        pos_eliminar := filePos(a_persona) - 1 ;
+        seek(a_persona, pos_eliminar);
+        negar_registro(r_aux, libre);
+        write(a_persona, r_aux);
+        negar_registro(r_aux, pos_eliminar);
+        reset(a_persona);
+        write(a_persona, r_aux);
+        libre := r_aux.prox;
+        close(a_persona);
+        fallo := false;
+      end;
     end;
   end;
 
-  procedure Modificar(var a:vieja; n_nombre:String; n_dni:Integer; n_f_nac:Longword);
+  procedure Modificar(var a:r_control; n_nombre:String; dni:Integer; n_f_nac:Longword);
   begin
     Recuperar(a, dni);
     if (not fallo_ultima_operacion(a)) then begin
       with (a) do begin
         seek(a_persona, filePos(a_persona) - 1);
         with (r_aux) do begin
-          dni    := n_dni;
+          dni    := dni;
           nombre := n_nombre;
           f_nac  := n_f_nac;
         end;
         write(a_persona, r_aux);
-        close(a_persona):
+        close(a_persona);
       end;
     end;
   end;
 
-(* ========================================================= *)
-(* =====================Privados============================ *)
-(* ========================================================= *)
+  Procedure Respaldar(var a:r_control; n_archivo:String);
+  var
+    n_arch : f_persona;
+  begin
+    Assign(n_arch, n_archivo);
+    Rewrite(n_arch);
+  end;
+End.
 
-  Function fallo_ultima_operacion(var a:vieja): Boolean;
-  begin
-    fallo_ultima_operacion := a.fallo;
-  end;
-
-  procedure leer_registro(var a:a_persona; var r:r_persona);
-  begin
-    if (not eof(a)) then begin
-      read(a, r);
-    end else begin
-      r.dni := corte;
-    end;
-  end;
-  procedure escupir_a_txt(var txt:Text; reg:r_persona);
-  begin
-    with (reg) do begin
-      write(txt, dni);
-      write(txt, f_nac);
-      writeln(txt, nombre);
-      (* TODO: ver si no se puede hacer todo en un solo writeln *)
-    end;
-  end;
-End;
